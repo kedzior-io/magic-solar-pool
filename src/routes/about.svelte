c48a8949b79e6cc157046eef57ff621ed9d2e1c2
Artur Kedzior <artur@kedzior.es>
2022-07-29 00:31:59 +0200

    project setup

---
+++ src/routes/about.svelte
@@ -0,0 +1,52 @@
+<script context="module">
+	import { browser, dev } from '$app/env';
+
+	// we don't need any JS on this page, though we'll load
+	// it in dev so that we get hot module replacement...
+	export const hydrate = dev;
+
+	// ...but if the client-side router is already loaded
+	// (i.e. we came here from elsewhere in the app), use it
+	export const router = browser;
+
+	// since there's no dynamic data here, we can prerender
+	// it so that it gets served as a static asset in prod
+	export const prerender = true;
+</script>
+
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="content">
+	<h1>About this site</h1>
+	
+	<h2>Who am I</h2>
+
+	<p>
+		My name is <a href="https://twitter.com/KedziorArtur">Artur Kedzior</a> and I'm a software engineer based in Spain. 
+		I created this site to help people make their own <strong>solar pool heaters</strong> just as I did with my pool.
+	</p>
+
+	<h2>But why?</h2>
+
+	<p>
+		My pool is in the mountain house at 995m (XXXft) above the sea level in the province of Alicante, Spain. 
+		Although it's hot in here during the summer, the pool temperature usually stays between 25-27C (x-x F) which is <strong>freaking cold</strong>! 
+	</p>
+
+	<h2>What did I do?</h2>
+
+	<p>
+		I have watched all the videos of this amazing dude (<a href="https://www.youtube.com/user/gabrielgroso/videos">SolarHeatOK</a>) which shared all his findings through experimenting and I thought it will be fun to put this all together. 
+	</p>
+</div>
+
+<style>
+	.content {
+		width: 100%;
+		max-width: var(--column-width);
+		margin: var(--column-margin-top) auto 0 auto;
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,89 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+	import Counter from '$lib/Counter.svelte';
+
+	let unitTypes  = [
+		{ id: 1, text: `meters` },
+		{ id: 2, text: `feet` },
+	];
+
+	let unit: number;
+
+	let poolWidth: number;
+	let poolLenght: number;
+	let poolDepth: number;
+
+	let test: string;
+
+</script>
+
+<svelte:head>
+	<title>Magic Solar Power</title>
+	<meta name="description" content="The best free resource to learn how to build DIY solar pool heating" />
+</svelte:head>
+
+<section>
+	<h1>
+		Build your own solar pool heating
+	</h1>
+
+	<h3>
+		The best <strong>free</strong> resource to learn how to build DIY solar pool heating.
+	</h3>
+
+	<h5>
+		Magic Solar Pool will recommend what materials you should get and why, how much material you need and where you could get it. 
+		The intention is to help people build solar pool heating the cheapest and the most efficient way possible. 
+	</h5>
+
+	<!-- <form class="content">
+		<label for="">Units</label>
+		<select value={unit}>
+			{#each unitTypes as unit}
+				<option value={unit.id}>
+					{unit.text}
+				</option>
+			{/each}
+		</select>
+		<br/>
+		<label for="">Nome</label>
+		<input type="text" bind:value={test} />
+	</form> -->
+<h5>Pool Size</h5>
+<div style="display: flex; 	align-items: center;">
+	<div>
+		<Counter title="Length" bind:count={poolLenght} />
+	</div>
+	<div>
+		<Counter title="Width" bind:count={poolWidth}  />
+	</div>
+	<div>
+		<Counter title="Depth" bind:count={poolDepth}  />
+	</div>
+</div>
+<h2>{poolLenght * poolWidth * poolDepth * 1000} liters</h2>
+
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	select {
+		padding: 2px;
+		font-size: 1rem;
+	}
+
+</style>
