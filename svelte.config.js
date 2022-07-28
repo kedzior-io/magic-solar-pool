fb51b4a137cebbee57a60e28c636c6759b4ee18c
Artur Kedzior <artur@kedzior.es>
2022-07-29 00:58:10 +0200

    clean up

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
 	preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter(),
-
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
-	}
+    kit: {
+        adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: true
+        }),
+    },
+	paths: {
+		base: '/wwwroot',
+	},
 };
 
 export default config;
